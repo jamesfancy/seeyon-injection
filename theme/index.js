5870d9d5663c0b68a6e828505e29e1914e847a8c
James Fan <jamesfancy@126.com>
2020-12-18 15:05:59 +0800

    改成 function，不自动执行

--- theme/index.js
+++ theme/index.js
@@ -1,9 +1,7 @@
-console.log("inject themejs for oa.kiloway.cn, by James");
-
-(() => {
+window.injectCustomTheme = function () {
     const { layoutName } = window.vPortal || {};
 
     if (layoutName) {
         document.body.classList.add("jfan-layout", layoutName);
     }
-})();
+};
